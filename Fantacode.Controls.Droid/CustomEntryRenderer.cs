6bc7c77832c954beebac2ce8afa89de0576368ec
Khadeeja Alif <khadeeja@fantacode.com>
2019-02-20 18:20:52 +0530

    Entry long click handled

--- Fantacode.Controls.Droid/CustomEntryRenderer.cs
+++ Fantacode.Controls.Droid/CustomEntryRenderer.cs
@@ -2,6 +2,7 @@
 using Android.Content;
 using Fantacode.Controls;
 using Fantacode.Controls.Droid;
+using Java.Lang;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -28,5 +29,7 @@ namespace Fantacode.Controls.Droid
                 SetPadding(0, 0, 0, 0);
             }
         }
+
+        public override bool PerformLongClick()         {             try             {                 return base.PerformLongClick();             }             catch(NullPointerException e)             {                 return true;             }             catch(System.Exception e)             {                 return true;             }         }          public override bool PerformLongClick(float x, float y)         {             try             {                 return base.PerformLongClick(x, y);             }             catch (NullPointerException e)             {                 return true;             }             catch (System.Exception e)             {                 return true;             }         }
     }
 }
